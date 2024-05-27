186d561a45d5d3083e08dcee49a47418649e6c3e
IvanWinataa <146960260+IvanWinataa@users.noreply.github.com>
2024-05-27 21:47:17 +0800

    Add files via upload

---
+++ AplikasiDonorDarah/src/aplikasidonordarah/AplikasiDonorDarah.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package aplikasidonordarah;
+
+/**
+ *
+ * @author ASUS TUF
+ */
+public class AplikasiDonorDarah {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        ChoosePage ChoosePageFrame = new ChoosePage();
+        ChoosePageFrame.setVisible(true);
+        ChoosePageFrame.pack();
+        ChoosePageFrame.setLocationRelativeTo(null);
+    }
+    
+}
